ab48702347f7ef138e05096633d2fe117fac5377
palisabhat <bhatpalisa2012@gmail.com>
2025-06-27 14:02:10 -0700

    Update FinalProject_BootCamp1.py

--- FinalProject_BootCamp1.py
+++ FinalProject_BootCamp1.py
@@ -15,14 +15,36 @@ bossList = ["The Wizard of DEATH", "Dr. Distster", "Dragon King", "Mistress Pois
 
 time.sleep(10)
 
+bossList = ["Dragon King", "Shadow Witch", "Cyber Golem", "Toxic Hydra"]
+
 counter = 0
 while counter < 1:
-    print("Revealing the final boss...")
-    counter = counter + 1
+    print("Choose the boss you want to battle...")
+    print("1. Dragon King")
+    print("2. Shadow Witch")
+    print("3. Cyber Golem")
+    print("4. Toxic Hydra")
 
-boss = "The wizard of DEATH"
+    choice = input("Enter the number: ")
+
+    if choice == "1":
+        boss = "Dragon King"
+        counter = counter + 1
+    elif choice == "2":
+        boss = "Shadow Witch"
+        counter = counter + 1
+    elif choice == "3":
+        boss = "Cyber Golem"
+        counter = counter + 1
+    elif choice == "4":
+        boss = "Toxic Hydra"
+        counter = counter + 1
+    else:
+        print("That is not a valid option. Try again.")
+
 time.sleep(2)
 boss_power = 10
+print("You will battle " + boss + " with a power level of " + str(boss_power))
 
 time.sleep(2)
 player_power = random.randint(2, 8)
@@ -63,5 +85,15 @@ else:
     time.sleep(5)
     gold = gold + 10
     print("You get 10 gold.")
+    print("You find 3 mysterious treasure chests...")
+i = 1
+while i <= 3:
+    print("Opening chest number " + str(i))
+    time.sleep(2)
+    coins = random.randint(5, 15)
+    print("You found " + str(coins) + " coins")
+    gold = gold + coins
+    i = i + 1
 
-print("End of Level 1.")
+print("You now have " + str(gold) + " gold")
+print("Congratulations on making it to the end of Level 1.")
